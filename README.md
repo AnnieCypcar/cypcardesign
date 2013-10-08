8f6b94ba50805893b757755eb02bca1fdba96071
AnnieCypcar <anne@cypcar.com>
2013-10-08 13:08:25 -0400

    fixup

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ cypcardesign
 ============
 
 Annie Cypcar's Portfolio Site
+
+Re-writing and revising JavaScript for www.cypcardesign.com.
--- scripts/Gruntfile.js
+++ scripts/Gruntfile.js
@@ -2,15 +2,6 @@ module.exports = function(grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-//        uglify: {
-//            options: {
-//                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-//            },
-//            build: {
-//                src: 'src/<%= pkg.name %>.js',
-//                dest: 'build/<%= pkg.name %>.min.js'
-//            }
-//        },
         watch: {
             source: {
                 files: ['script.js'],
@@ -18,11 +9,8 @@ module.exports = function(grunt) {
             }
         },
         jshint: {
-            // define the files to lint
             files: ['Gruntfile.js', 'script.js'],
-            // configure JSHint (documented at http://www.jshint.com/docs/)
             options: {
-                // more options here if you want to override JSHint defaults
                 globals: {
                     jQuery: true,
                     console: true,
@@ -32,11 +20,9 @@ module.exports = function(grunt) {
         }
     });
 
-//    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    // Default task(s).
-   grunt.registerTask('default', ['watch']);
+    grunt.registerTask('default', ['watch']);
 
 };
